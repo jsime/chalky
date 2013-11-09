2abc85b0ae1818c607ddb343fe3824824de8cba6
Jon Sime <jonsime@gmail.com>
2013-11-09 01:28:53 -0500

    apparently this is the convention

--- chalky.js
+++ chalky.js
@@ -1,6 +1,6 @@
-var http = require("http");
+var chalky = require("http");
 
-http.createServer(function(req, res) {
+chalky.createServer(function(req, res) {
     res.writeHead(200, { "Content-Type": "text/plain" });
     res.write("Chalky Placeholder");
     res.end();
